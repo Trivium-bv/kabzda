9c74708cc592ae5b7e88407d946e4de7aec21008
Trivium-bv <trivium.bv@gmail.com>
2025-03-18 13:26:25 +0300

    destructuring lesson 01

---
+++ src/tests/02-destructuring.test.ts
@@ -0,0 +1,22 @@
+test("", () => {
+    const man = {
+        name: "Dimych",
+        age: 32,
+        lessons: [{title: "1"}, {title: "2"}],
+        adress: {
+            street: {
+                title: "Nezavisimosti street"
+            }
+        }
+    }
+
+// const age = man.age;
+// const lessons = man.lessons;
+const {age, lessons} = man;
+
+const {title} = man.adress.street
+
+    expect(age).toBe(32);
+    expect(lessons.length).toBe(2);
+    expect(title.length).toBe("Nezavisimosti street");
+})
